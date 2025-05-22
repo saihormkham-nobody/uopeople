012420c49148f906e91534ef5a670348eda119c9
saihormkham-nobody <saihormkham1998@gmail.com>
2025-05-22 12:06:10 +0900

    unit7: javafx project init

---
+++ cs1102-01/unit7/student-mangement-system/src/main/java/cs1102/unit7/PrimaryController.java
@@ -0,0 +1,12 @@
+package cs1102.unit7;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ cs1102-01/unit7/student-mangement-system/src/main/java/cs1102/unit7/SecondaryController.java
@@ -0,0 +1,12 @@
+package cs1102.unit7;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
